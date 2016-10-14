91d509aa29eaded5d11b60687177802537c08b68
sevdalinapashova91 <sevdalina.pashova@yahoo.com>
2016-10-14 10:51:22 +0300

    //creating UniqueIdGenerator for singleton illustration

---
+++ CreationalPatternsExamples/SingletonPattern/UniqueIdGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class UniqueIdGenerator
+    {
+        private static volatile UniqueIdGenerator uniqueIdInstance;
+        private static object syncLock = new object();
+        private int uniqueId = 0;
+        private UniqueIdGenerator() { }
+        public static UniqueIdGenerator Instance
+        {
+            get
+                {
+                if (uniqueIdInstance == null)
+                {
+                    lock (syncLock)
+                    {
+                        if (uniqueIdInstance == null)
+                        {
+                            uniqueIdInstance = new UniqueIdGenerator();
+                        }
+                    }
+                }
+                return uniqueIdInstance;
+            }
+        }
+        public int GetUniqueId()
+        {
+            return uniqueId++;
+        }
+    }
+}
